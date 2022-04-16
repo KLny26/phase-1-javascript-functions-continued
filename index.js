bd5419b1a84d8c3bd4ca186bc53201045e5748f8
kristenli <kyli2005@gmail.com>
2022-04-16 16:04:06 -0400

    commit tree 063dc69593aebf66588020b261830ea1832a6203

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+return(`This Saturday, I want to ${activity}!`);
+}
+
+function mondayWork(method = "go to the office"){
+    return(`This Monday, I will ${method}.`);
+}
+
+function wrapAdjective (meow ="*"){
+    return function (typeOfWorker = "a hard worker"){
+        return(`You are ${meow}${typeOfWorker}${meow}!`);
+    }
+}
